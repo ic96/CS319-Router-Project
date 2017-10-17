a63f3e4537b66ac5110e9c1f38c52f6d9c5b6cdd
Edward Huang <edward.yh.huang@outlook.com>
2017-10-16 20:50:40 -0700

    Added /userdevices/<company_recid> functionality
    Added /userdevices/<company_recid> to retrieve a list of devices from all sites of that company.

--- server/api/views.py
+++ server/api/views.py
@@ -28,3 +28,57 @@ def device_status(request, device_id):
     }
 
     return HttpResponse(json.dumps(res))
+
+def customer_devices(request, customer_id):
+	sites = models.MspSite.objects.filter(company_recid=customer_id)
+	site_list = []
+	for site in sites:
+		site_id = site.site_recid
+		site_description = site.description
+		site_address1 = site.address1
+		site_address2 = site.address2
+		site_city = site.city
+		site_province = site.province
+		site_postal_code = site.postal_code
+		site_latitude = site.latitude
+		site_longitude = site.longitude
+		
+		devices = models.MspDevice.objects.filter(site_recid=site_id)
+		device_list = []
+		for device in devices:
+			device_recid = device.device_recid
+			device_id = device.device_id
+			device_manufacturer = device.manufacturer
+			device_description = device.description
+			device_type = device.device_type
+			device_mac_address = device.mac_address
+			device_ip_address = device.ip_address
+						
+			device_res = {
+				'device_recid': device.device_recid,
+				'device_id': device.device_id,
+				'device_manufacturer': device.manufacturer,
+				'device_description': device.description,
+				'device_type': device.device_type,
+				'device_mac_address': device.mac_address,
+				'device_ip_address': device.ip_address
+			}
+			
+			device_list.append(device_res)
+			
+		site_res = {
+			'site_id': site.site_recid,
+			'site_description': site.description,
+			'site_address1': site.address1,
+			'site_address2': site.address2,
+			'site_city': site.city,
+			'site_province': site.province,
+			'site_postal_code': site.postal_code,
+			'site_latitude': site.latitude,
+			'site_longitude': site.longitude,
+			'site_devices': device_list
+		}
+	
+		site_list.append(site_res)
+		
+	return HttpResponse(json.dumps(site_list))
