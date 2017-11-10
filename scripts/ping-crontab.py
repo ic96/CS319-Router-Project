367610e3596377554485d4d977a990c837d271f6
Edward Huang <edward.yh.huang@outlook.com>
2017-11-10 14:50:14 -0800

    Cronjob scripts.
    Added a crontab file gitgood_crontab that calls the ping-crontab.py to ping once on each device every minute.

--- scripts/ping-crontab.py
+++ scripts/ping-crontab.py
@@ -2,7 +2,7 @@ import psycopg2
 import subprocess
 import re
 
-get_device_data = "SELECT device_id, ip_address FROM msp_device;"
+get_device_data = "SELECT device_recid, ip_address FROM msp_device;"
 insert_data_capture = """
     INSERT INTO msp_data_capture
     (device_recid, latency_milliseconds, responded)
@@ -11,17 +11,17 @@ insert_data_capture = """
 
 def ping_host(host, device_id):
     print('Pinging {0}'.format(host));
-    p = subprocess.Popen(['ping', '-c', '5', host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(['ping', '-c', '1', host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     if err:
         print(err)
     else:
-        matcher = re.compile("round-trip min/avg/max/stddev = (\d+.\d+)/(\d+.\d+)/(\d+.\d+)/(\d+.\d+)")
+        matcher = re.compile("rtt min/avg/max/mdev = (\d+.\d+)/(\d+.\d+)/(\d+.\d+)/(\d+.\d+)")
         res = matcher.search(out).groups()
         responded = False
         latency = None
         if res:
-            print('Avg latency for {0} = {1} ms'.format(host, res[1]))
+            print('Latency for {0} = {1} ms'.format(host, res[1]))
             responded = True
             latency = res[1]
             cur.execute(insert_data_capture.format(device_id, latency, responded))
