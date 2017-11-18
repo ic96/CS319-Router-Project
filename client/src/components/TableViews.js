67e3a271012ef9fbe80a9d1d9ef65cb3be32728e
Ian <ian.chan96@hotmail.com>
2017-11-17 19:48:43 -0800

    Table and subtable data

--- client/src/components/TableViews.js
+++ client/src/components/TableViews.js
@@ -7,55 +7,129 @@ import { makeData, Logo, Tips } from "../Utils";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 
+const columns = [
+    {
+        columns: [
+            {
+                Header: "Name",
+                accessor: "site_address1"
+            },
+            {
+                Header: "Usage",
+                id: "usage",
+                accessor: d => d.usage
+            }
+        ]
+    },
+    {
+        columns: [
+            {
+                Header: "Clients",
+                accessor: "clients"
+
+
+            }
+        ]
+    },
+    {
+        columns: [
+            {
+                Header: "Network Type",
+                accessor: "network_type"
+            }
+        ]
+    },
+    {
+        columns: [
+            {
+                Header: "Network Health",
+                accessor: "network_health"
+            }
+        ]
+    }
+];
+
+const subColumns = [
+    {
+        columns: [
+            {
+                Header: "Name",
+                accessor: "device_description"
+            }
+        ]
+    },
+    {
+        columns: [
+            {
+                Header: "IP Address",
+                accessor: "device_ip_address"
+
+
+            }
+        ]
+    },
+    {
+        columns: [
+            {
+                Header: "Network Type",
+                accessor: "device_type"
+            }
+        ]
+    },
+    {
+        columns: [
+            {
+                Header: "Latency",
+                accessor: "latency"
+            }
+        ]
+    }
+];
+
+const SubTable = ({data}) => {
+    return(
+        data.map(sites =>
+        sites.site_devices.map(devices =>
+            <ReactTable
+                data={devices}
+                columns={subColumns}
+                defaultPageSize={3}
+                showPagination={false}
+            />
+        )
+    )
+)
+}
+
 export default class TableView extends React.Component {
 
 
-  
   render() {
     const { data } = this.props;
-    console.log(data);
+      var devicesArray = [];
     return (
       <div>
         <ReactTable
           data={data}
-          columns={[
-            {
-              columns: [
-                {
-                  Header: "Name",
-                  accessor: "site_address1"
-                },
-                {
-                  Header: "Usage",
-                  id: "usage",
-                  accessor: d => d.usage
-                }
-              ]
-            },
-            {
-              columns: [
-                {
-                  Header: "Clients",
-                  accessor: "clients"
-                },
-                {
-                  Header: "Tags",
-                  accessor: "tags"
-                }
-              ]
-            },
-            {
-              columns: [
-                {
-                  Header: "Network Type",
-                  accessor: "network_type"
-                }
-              ]
-            }
-          ]}
+          columns={columns}
           defaultPageSize={10}
           className="-striped -highlight"
-        />
+          SubComponent={row => {
+              const siteDevices = row.original.site_devices;
+              console.log(siteDevices);
+              return (
+                  <div style={{padding: "20px"}}>
+                      <ReactTable
+                      data={siteDevices}
+                      columns={subColumns}
+                      defaultPageSize={10}
+                      showPagination={false}
+                      />
+
+                  </div>
+              );
+          }}
+          />
         <br />
         <Tips />
         <Logo />
@@ -63,3 +137,4 @@ export default class TableView extends React.Component {
     );
   }
 }
+
