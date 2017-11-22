97229a4f7945a84e1be6ba3048395221af92e8d7
BuildTools <unconfigured@null.spigotmc.org>
2017-11-22 13:11:15 -0800

    Added strings to execute

--- scripts/location.py
+++ scripts/location.py
@@ -1,6 +1,8 @@
 import psycog2
 
-command = "SELECT * from msp_site;"
+get_sites = "SELECT site_recid, address1, city, postal_code from msp_site;"
+call_api = ""
+api_key = "AIzaSyBGgneGpOxeRSzKytQDSjQuz-owUnjk-Mc"
 
 try:
     conn = psycopg2.connect(dbname="postgres", user="postgres", password="")
