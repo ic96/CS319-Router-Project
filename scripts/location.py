c55dd9894f0192121dcfdb0b83dbe3c649dad968
BuildTools <unconfigured@null.spigotmc.org>
2017-11-22 16:06:23 -0800

    added json parsing and googlemaps api

--- scripts/location.py
+++ scripts/location.py
@@ -1,12 +1,22 @@
-import psycog2
+import psycopg2
+import googlemaps
+import json
+
+api_key = "AIzaSyBGgneGpOxeRSzKytQDSjQuz-owUnjk-Mc"
+gmaps = googlemaps.Client(key=api_key)
 
 get_sites = "SELECT site_recid, address1, city, postal_code from msp_site;"
 call_api = ""
-api_key = "AIzaSyBGgneGpOxeRSzKytQDSjQuz-owUnjk-Mc"
 
-try:
-    conn = psycopg2.connect(dbname="postgres", user="postgres", password="")
-    cur = conn.cursor()
+result = gmaps.geocode('77 Edgevalley close')
+
+#data = json.loads(json.dumps(result))
+#print(json.dumps(result, sort_keys = True))
+print(result[0]["geometry"]["location"])
+
+#try:
+ #   conn = psycopg2.connect(dbname="postgres", user="postgres", password="")
+  #  cur = conn.cursor()
     
 
 
