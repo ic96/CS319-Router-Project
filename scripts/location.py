8d534b649ce4ef4bb41a91f1baae24f2f635f28e
BuildTools <unconfigured@null.spigotmc.org>
2017-11-23 00:26:29 -0800

    Added sql iteration

--- scripts/location.py
+++ scripts/location.py
@@ -6,18 +6,37 @@ api_key = "AIzaSyBGgneGpOxeRSzKytQDSjQuz-owUnjk-Mc"
 gmaps = googlemaps.Client(key=api_key)
 
 get_sites = "SELECT site_recid, address1, city, postal_code from msp_site;"
-call_api = ""
 
-result = gmaps.geocode('77 Edgevalley close')
+result = gmaps.geocode('77 Edgevalley Close T3A5E9')
 
-#data = json.loads(json.dumps(result))
-#print(json.dumps(result, sort_keys = True))
-print(result[0]["geometry"]["location"])
+#print(result[0]["geometry"]["location"]["lat"])
+#print(result[0]["geometry"]["location"]["lng"])
 
-#try:
- #   conn = psycopg2.connect(dbname="postgres", user="postgres", password="")
-  #  cur = conn.cursor()
-    
+try:
+   conn = psycopg2.connect(dbname="ubc05", user="louis", password="123")
+   cur = conn.cursor()
+   cur.execute(get_sites)
+  
+   for line in cur:
+      print(line)
+      site = line[0]
+      address = line[1]
+      postal = line[3]
+      result = gmaps.geocode(address + ' ' + postal)
+      if len(result) >= 1:
+      #   latitude = result[0]["geometry"]["lat"]
+      #   longitude = result[0]["geometry"]["lng"]
+         print(result[0]["geometry"])
+#         update_query = "update msp_site SET (latitude,longitude) = ("+latitude+","+longitude+") where site_recid = " + site + ";" 
+      else:
+        print(address+" Got No result")
+
+except Exception as e:
+       print(e)
+finally:
+       if conn is not None:
+                  conn.close()
+   
 
 
 
