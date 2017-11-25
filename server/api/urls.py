f4543e3c6c53c9b202da6f86dbf2873cd58927ce
Edward Zhou Muhua <edwardzhou1996@outlook.com>
2017-11-24 18:56:36 -0800

    change api url

--- server/api/urls.py
+++ server/api/urls.py
@@ -3,10 +3,10 @@ from rest_framework.authtoken import views as drf_views
 from views import Home, device_status, customer_devices, device_history, login
 
 urlpatterns = [
+    url(r'^userlogin/$', login, name='userlogin'),
     url(r'^device/(?P<device_id>[0-9]+)/$', device_status, name='device_status'),
 	url(r'^userdevices/(?P<customer_id>[0-9]+)/$', customer_devices, name='customer_devices'),
     url(r'^devicehistory/(?P<device_id>[0-9]+)/$', device_history, name='device_history'),
-    url(r'^login$', login, name='login'),
     url(r'^', Home.as_view(), name='home'),
     url(r'^auth$', drf_views.obtain_auth_token, name='auth'),
 ]
